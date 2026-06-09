93917a86aa4d3367ce84120add284cf674c84ee3
Dane Fitzenreider <danerf2@illinois.edu>
2026-06-09 12:39:58 -0500

    Create main.py for ION-I Optics simulations
    Initial setup for ION-I Optics grids simulations with FEMM.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+This is just a starting document to get some foundational code layed out. Feel free to edit as you wish and make large revision, I just wanted to get things started. Good luck everyone!
+"""
+
+#ION-I Optics grids simulations
+import femm
+
+#Open FEMM and create electrostatic document
+femm.openfemm()
+femm.newdocument('electrostatics')
+
